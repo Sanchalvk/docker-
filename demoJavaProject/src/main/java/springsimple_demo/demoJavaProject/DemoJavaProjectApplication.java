96b2ab940bae151d07a13938401136545da1d3ad
Sanchalvk <khedkarsanchal86@gmail.com>
2025-04-21 23:16:40 -1100

    docker

---
+++ demoJavaProject/src/main/java/springsimple_demo/demoJavaProject/DemoJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package springsimple_demo.demoJavaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaProjectApplication.class, args);
+	}
+
+}
